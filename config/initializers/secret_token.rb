c219ca322f6a1bc15e246458dbf87728fca8d53a
James "The Beast" West <zxcvbgfdsa8@gmail.com>
2013-01-12 11:16:59 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RentalManager::Application.config.secret_token = '2da13ab690d2d19c3c90023ce956fe10290d171c35b34dc502344b240c5e7c038fd9ab202273cff66f72cced59e5b24b6f6747a427f3223c59e5b65ddf4d422f'
