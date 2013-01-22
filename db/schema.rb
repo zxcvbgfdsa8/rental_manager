341f449fc1aebffbe014da685b9471ec42b48a05
James "The Beast" West <zxcvbgfdsa8@gmail.com>
2013-01-22 23:28:24 +0100

    adding telephon numbers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130112104420) do
+ActiveRecord::Schema.define(:version => 20130122221815) do
+
+  create_table "phone_numbers", :force => true do |t|
+    t.string   "number"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "salaries", :force => true do |t|
     t.string   "type"
